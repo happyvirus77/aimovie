a3f08b7b89d62db1e59954236dae881a99dac685
happyvirus7777 <happyviruscom7777777@gmail.com>
2026-05-23 18:22:39 +0900

    fix github pages router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import './App.css';
-import App from './App.jsx';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { HashRouter } from 'react-router-dom'
+import App from './App'
 
-createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <HashRouter>
     <App />
-  </React.StrictMode>
-);
+  </HashRouter>
+)
